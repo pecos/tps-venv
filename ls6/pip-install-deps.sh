f5552d4eb8827fa9c139f81cfe098090390dd4ff
Umberto Villa <uvilla@oden.utexas.edu>
2024-03-19 11:15:58 -0500

    Fix MPICXX compiler

--- ls6/pip-install-deps.sh
+++ ls6/pip-install-deps.sh
@@ -5,7 +5,7 @@ pip install cython psutil scikit-build nvtx
 pip install numpy scipy  matplotlib cupy-cuda12x numba multiprocess "pybind11[global]" lxcat_data_parser
 
 MPICC=$(command -v mpicc)
-MPICXX=$(command -v mpig++)
+MPICXX=$(command -v mpicxx)
 cd $WDIR
 wget https://github.com/mpi4py/mpi4py/releases/download/$mpi4py_ver/mpi4py-$mpi4py_ver.tar.gz \
 && tar -zxf mpi4py-$mpi4py_ver.tar.gz && cd mpi4py-$mpi4py_ver \
