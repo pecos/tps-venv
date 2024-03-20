1b2187f5bf05edcb0b0945ea1dbea060492a6ef2
milindasf <milinda@cs.utah.edu>
2024-03-20 15:01:33 -0500

    create sbatch script added for ls6

---
+++ ls6/create-sbatch.sh
@@ -0,0 +1,76 @@
+#!/bin/bash -x
+WDIR=$(pwd)
+TPS_DIR=$WDIR/tps
+TPS_INPUTS_DIR=$WDIR/tps/tps-inputs/axisymmetric/argon/lowP/six-species-maxwell-rates
+cd $TPS_INPUTS_DIR
+
+echo $(pwd)
+ls
+
+python3 gen_par.py --ngpus_per_node=3 --solver_type="steady-state" --out_fname="ss" --lxcat=$WDIR/tps/boltzmann/BESolver/python/lxcat_data/eAr_crs.6sp_Tg_0.5eV -sub_clusters 256 256 128 64 32 16 8 4 -node_count 1 2 4 8 16 32 64 128
+
+#python3 gen_par.py --ngpus_per_node=3 --solver_type="transient" --out_fname="ts" --lxcat=$WDIR/tps/boltzmann/BESolver/python/lxcat_data/eAr_crs.6sp_Tg_0.5eV -sub_clusters 256 256 128 64 32 16 8 4 -node_count 1 2 4 8 16 32 64 128
+
+cd $WDIR
+
+cat >run.sbatch<<EOF
+#----------------------------------------------------
+# Sample Slurm job script
+#   for TACC Frontera CLX nodes
+#
+#   *** MPI Job in Normal Queue ***
+# 
+# Last revised: 20 May 2019
+#
+# Notes:
+#
+#   -- Launch this script by executing
+#      "sbatch clx.mpi.slurm" on a Frontera login node.
+#
+#   -- Use ibrun to launch MPI codes on TACC systems.
+#      Do NOT use mpirun or mpiexec.
+#
+#   -- Max recommended MPI ranks per CLX node: 56
+#      (start small, increase gradually).
+#
+#   -- If you're running out of memory, try running
+#      fewer tasks per node to give each task more memory.
+#
+#----------------------------------------------------
+
+#SBATCH -J tps-bte         # Job name
+#SBATCH -o .tps-bte-.o%j   # Name of stdout output file
+#SBATCH -e .tps-bte-.e%j   # Name of stderr error file
+#SBATCH -t 01:30:00        # Run time (hh:mm:ss)
+#SBATCH --mail-type=all    # Send email at begin and end of job
+##SBATCH --mail-user=username@tacc.utexas.edu
+
+# Any other commands must follow all #SBATCH directives...
+source load_modules.sh
+source tps-env/bin/activate
+source tps-env/export_env
+
+module list
+pwd
+date
+
+WDIR=\$(pwd)
+TPS_DIR=\$WDIR/tps
+
+cd \$TPS_DIR
+mkdir -p build-gpu && cd build-gpu
+make all -j16
+cd ../ && cp src/*.py build-gpu/src/.
+
+cd \$WDIR
+PAR_FILE_HOME=\$TPS_DIR/tps-inputs/axisymmetric/argon/lowP/six-species-maxwell-rates
+cd \$PAR_FILE_HOME
+git checkout restart_output-plasma.sol.h5
+
+python3 --version
+export PARLA_NUM_THREADS=8  
+
+ibrun ./../../../../../build-gpu/src/tps-bte_0d3v.py -run plasma.6sp.tps2boltzmann_ss_$1.ini
+cd \$WDIR
+EOF
+# chmod +x run.sbatch
