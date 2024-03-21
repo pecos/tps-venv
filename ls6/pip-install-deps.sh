1539255fe9c736dedf8ed9bd67a1a1b518d83519
Umberto Villa <uvilla@oden.utexas.edu>
2024-03-21 15:36:34 -0500

    Make pip-install-deps.sh a running script

--- ls6/pip-install-deps.sh
+++ ls6/pip-install-deps.sh
@@ -1,10 +1,22 @@
-source $INSTALL_DIR/bin/activate
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR=$(pwd)
+INSTALL_DIR=$ROOT_DIR/tps-env
+WDIR=$ROOT_DIR/build
+make_cores=6
+
+set -e
+
+source $SCRIPT_DIR/load_modules.sh
+source $INSTALL_DIR/bin/activate 
+source $INSTALL_DIR/export_env
 
 mpi4py_ver=3.1.5
 python --version
 pip install --upgrade pip
-pip install cython psutil scikit-build nvtx
+pip install cython==0.29.37 psutil scikit-build nvtx
 pip install numpy scipy  matplotlib cupy-cuda11x numba multiprocess "pybind11[global]" lxcat_data_parser
+
+python -c "import cupy"
 
 MPICC=$(command -v mpicc)
 MPICXX=$(command -v mpicxx)
@@ -12,12 +24,15 @@ cd $WDIR
 wget https://github.com/mpi4py/mpi4py/releases/download/$mpi4py_ver/mpi4py-$mpi4py_ver.tar.gz \
 && tar -zxf mpi4py-$mpi4py_ver.tar.gz && cd mpi4py-$mpi4py_ver \
 && python setup.py build --mpicc=$MPICC && python setup.py install
+cd $ROOT_DIR
+python -c "import mpi4py"
 
 cd $WDIR
 git clone git@github.com:ut-parla/parla-experimental.git
 cd parla-experimental
 git submodule update --init --recursive
 CC=$MPICC CXX=$MPICXX make all -j ${make_cores}
+cd $ROOT_DIR
+python -c "import parla"
 
-cd $SCRIPT_DIR
 #pip install --extra-index-url=https://pypi.nvidia.com cudf-cu11==24.2.* cuml-cu11==24.2.*
