26c7dc9fd2300b74c1e0da06f403bfcb54ec54f9
Umberto Emanuele Villa <villa13@tioga11.llnl.gov>
2024-03-21 11:20:35 -0700

    Fix pip0-install-deps.sh; We still need scikit-build

--- tioga/pip-install-deps.sh
+++ tioga/pip-install-deps.sh
@@ -15,7 +15,7 @@ source $INSTALL_DIR/export_env
 mpi4py_ver=3.1.5
 python --version
 pip install --upgrade pip
-pip install cython==0.29.37 psutil scikit-build-core 
+pip install cython==0.29.37 psutil scikit-build 
 pip install numpy scipy  matplotlib numba multiprocess "pybind11[global]" lxcat_data_parser
 
 MPICC=$(command -v mpicc)
@@ -23,14 +23,19 @@ MPICXX=$(command -v mpicxx)
 
 cd $WDIR
 wget https://github.com/mpi4py/mpi4py/releases/download/$mpi4py_ver/mpi4py-$mpi4py_ver.tar.gz \
-&& tar -zxf mpi4py-$mpi4py_ver.tar.gz && cd mpi4py-$mpi4py_ver \
+rm -rf mpi4py-$mpi4py_ver && tar -zxf mpi4py-$mpi4py_ver.tar.gz && cd mpi4py-$mpi4py_ver \
 && python setup.py build --mpicc=$MPICC && python setup.py install
+cd $ROOT_DIR
+python -c "import mpi4py"
 
 cd $WDIR
+rm -rf parla-experimental
 git clone git@github.com:ut-parla/parla-experimental.git
 cd parla-experimental
 git submodule update --init --recursive
 CC=$MPICC CXX=$MPICXX pip install . --config-settings=cmake.define.PARLA_ENABLE_HIP=ON 
+cd $ROOT_DIR
+python -c "import parla"
 
 export HCC_AMDGPU_TARGET=$hip_arch
 export CUPY_INSTALL_USE_HIP=1
@@ -39,5 +44,6 @@ export LD_LIBRARY_PATH=$ROCM_HOME/lib:$LD_LIBRARY_PATH
 echo $ROCM_HOME
 pip install cupy
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH_SWAP
+python -c "import cupy"
 
 cd $SCRIPT_DIR
