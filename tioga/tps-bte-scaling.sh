0faa0ce1e4cbfb08aec9dcb7eba5ed6dac7276a3
Umberto Emanuele Villa <villa13@tioga10.llnl.gov>
2024-03-26 08:21:43 -0700

    Updates to tps-bte-scaling.sh

--- tioga/tps-bte-scaling.sh
+++ tioga/tps-bte-scaling.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
 
+# Perform a strong scaling test of TPS+BTE
+# Please use tps-bte-create-input.sh to generate the input files
 # Submit as
-# for nn in 1 2 4 8 16; do flux submit -N $nn -n $(($nn*4))-t 1h --output=tps-bte_ss_${nn}.log ./tps-bte-scaling.sh $nn; done 
+# for nn in 1 2 4 8 16; do flux submit -N $nn -n $(($nn*4)) -t 1h --output=tps-bte_${nn}.log ./tps-bte-scaling.sh $nn; done 
 WDIR=$(pwd)
 TPS_DIR=$WDIR/tps
 TPS_INPUTS_DIR=$WDIR/tps/tps-inputs/axisymmetric/argon/lowP/six-species-maxwell-rates
@@ -27,8 +29,8 @@ cd $PAR_FILE_HOME
 [[ $FLUX_TASK_RANK -eq 0 ]] && git checkout restart_output-plasma.sol.h5
 [[ $FLUX_TASK_RANK -eq 0 ]] && git checkout ../six-species-maxwell-rates-x4/restart_output-plasma-x4.sol.h5
 [[ $FLUX_TASK_RANK -eq 0 ]] && git checkout ../six-species-maxwell-rates-x16/restart_output-plasma-x16.sol.h5
-[[ $FLUX_TASK_RANK -eq 0 ]] && ln -s ../six-species-maxwell-rates-x4/restart_output-plasma-x4.sol.h5 . 
-[[ $FLUX_TASK_RANK -eq 0 ]] && ln -s ../six-species-maxwell-rates-x16/restart_output-plasma-x16.sol.h5 . 
+[[ $FLUX_TASK_RANK -eq 0 ]] && ln -sf ../six-species-maxwell-rates-x4/restart_output-plasma-x4.sol.h5 . 
+[[ $FLUX_TASK_RANK -eq 0 ]] && ln -sf ../six-species-maxwell-rates-x16/restart_output-plasma-x16.sol.h5 . 
 
 # To run the steady state
 $TPS_DIR/build-gpu/src/tps-bte_0d3v.py -run plasma.6sp.tps2boltzmann.x16.ss.$1.ini 
